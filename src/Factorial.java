0a3a1f6e6baf9f04140623d696371f33d8ad3b09
Joe Gallego <josephjgallego@gmail.com>
2020-04-14 18:18:32 -0700

    adding examples

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,18 +1,37 @@
 /*
 
-Without using any Java or JavaScript native methods, create a function that returns the reverse of a string
-        - The function will take one parameter, a string
-        - The function will return the reverse of the string
-        - Try these test cases:
-        - reverseString("hello") -> should return 'olleh'
-        - reverseString("Coding Nomads") -> should return 'sdamoN gnidoC'
-        - reverseString("Greetings from Earth") -> should return "htraE morf sgniteerG"
-        - reverseString("I'm a little teapot") -> should return 'topeat elttil a m'I'
+3. Write a function that returns the factorial addition of a number
+( i.e.; 5 factorial  is 1 + 2 + 3 + 4 + 5 = 15 )
+    - The function will take one parameter, a number
+    - The function will return the factorial of that number
+    - Bonus: write the function as a recursive function
+    - Test cases:
+        - factorial(5) -> returns 15
+        - factorial(120) -> returns 55
+        - factorial(545) -> returns 120
+
 
 */
 
 public class Factorial {
 
- 
+    public static void main(String[] args) {
+        System.out.println();
+    }
 
+    public static int factorial(int x) {
+        int factorialSum = 0;
+        for (int i = 0; i <= x; i++) {
+            factorialSum += 1;
+        }
+        return factorialSum;
+    }
+
+    public static int recursiveFactorial(int x){
+        if (x == 0){
+            return 1;
+        }else{
+            return (x + recursiveFactorial(x -1));
+        }
+    }
 }
---
+++ src/StringReverser.java
@@ -0,0 +1,11 @@
+//Without using any Java or JavaScript native methods, create a function that returns the reverse of a string
+//        - The function will take one parameter, a string
+//        - The function will return the reverse of the string
+//        - Try these test cases:
+//        - reverseString("hello") -> should return 'olleh'
+//        - reverseString("Coding Nomads") -> should return 'sdamoN gnidoC'
+//        - reverseString("Greetings from Earth") -> should return "htraE morf sgniteerG"
+//        - reverseString("I'm a little teapot") -> should return 'topeat elttil a m'I'
+
+public class StringReverser {
+}
