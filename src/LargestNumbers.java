61b1eac71dafa3b29a7f1f102e6f127185624473
Joe Gallego <josephjgallego@gmail.com>
2020-04-14 18:22:03 -0700

    adding examples

---
+++ src/LargestNumbers.java
@@ -0,0 +1,15 @@
+/*
+
+5. Return Largest Numbers in Arrays
+    - The function will take in n array of arrays
+    - The function will return an array consisting of the largest number from each provided sub-array.
+    - Test cases:
+        - largestOfFour([[14, 35, 21, 53], [143, 427, 418, 246], [352, 355, 375, 359]]) should return [53, 427, 375].
+        - largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27, 5, 39, 1001]
+        - largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000]
+        - largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]) should return [25, 48, 21, -3]
+
+*/
+
+public class LargestNumbers {
+}
