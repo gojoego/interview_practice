a5d41758c4e178c7b5bc11f575e223535e655317
Joe Gallego <josephjgallego@gmail.com>
2020-04-26 19:00:23 -0700

    practice

---
+++ src/LoopPractice1.java
@@ -0,0 +1,47 @@
+/*
+
+Question 1
+
+Write a program to print numbers from 1 to 10.
+
+Question 2
+
+Write a program to calculate the sum of first 10 natural number.
+
+Question 3
+
+Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number.
+
+Question 4
+
+Write a program to find the factorial value of any number entered through the keyboard.
+
+Question 5
+
+Two numbers are entered through the keyboard. Write a program to find the value of one number raised to the power of another. (Do not use Java built-in method)
+
+Question 6
+
+Write a program that prompts the user to input an integer and then outputs the number with the digits reversed. For example, if the input is 12345, the output should be 54321.
+
+Question 7
+
+Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+
+Question 8
+
+Write a program that prompts the user to input a positive integer. It should then output a message indicating whether the number is a prime number.
+
+Question 9
+
+Write a program to calculate HCF of Two given number.
+
+Question 10
+
+Write a do-while loop that asks the user to enter two numbers. The numbers should be added and the sum displayed. The loop should ask the user whether he or she wishes to perform the operation again. If so, the loop should repeat; otherwise it should terminate.
+
+
+*/
+
+public class LoopPractice1 {
+}
