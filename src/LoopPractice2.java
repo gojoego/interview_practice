8c2e4e3b1973d1e473cafae4a97b6056987eb3d5
Joe Gallego <josephjgallego@gmail.com>
2020-05-06 13:11:56 -0700

    practice

--- src/LoopPractice2.java
+++ src/LoopPractice2.java
@@ -1,2 +1,27 @@
+/*
+
+Question 6
+
+Write a program that prompts the user to input an integer and then outputs the number with the digits reversed. For example, if the input is 12345, the output should be 54321.
+
+Question 7
+
+Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+
+Question 8
+
+Write a program that prompts the user to input a positive integer. It should then output a message indicating whether the number is a prime number.
+
+Question 9
+
+Write a program to calculate HCF of Two given number.
+
+Question 10
+
+Write a do-while loop that asks the user to enter two numbers. The numbers should be added and the sum displayed. The loop should ask the user whether he or she wishes to perform the operation again. If so, the loop should repeat; otherwise it should terminate.
+
+
+*/
+
 public class LoopPractice2 {
 }
