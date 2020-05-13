c92f8d9bca95fbdfc976b37cd7fcd1959724d8be
Joe Gallego <josephjgallego@gmail.com>
2020-05-13 00:47:10 -0700

    practice

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -95,6 +95,7 @@ public class Datatypes {
 
     public static void main(String[] args) {
 
+        dataTyper();
 
     }
 
@@ -103,20 +104,17 @@ public class Datatypes {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        for (
-                int i = 0;
-                i < t; i++) {
-
+        for(int i = 0; i < t; i++) {
             try {
                 long x = sc.nextLong();
                 System.out.println(x + " can be fitted in:");
                 if (x >= -128 && x <= 127) System.out.println("* byte");
-                //Complete the code
+                if (x >= -Math.pow(2,15) && x <= Math.pow(2,15)-1) System.out.println("* short");
+                if (x >= -Math.pow(2,31) && x <= Math.pow(2,31)-1) System.out.println("* int");
+                if (x >= -Math.pow(2,63) && x <= Math.pow(2,63)-1) System.out.println("* long");
             } catch (Exception e) {
                 System.out.println(sc.next() + " can't be fitted anywhere.");
             }
-
         }
     }
-    
 }
