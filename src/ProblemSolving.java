1c6d2582cf94ee86d1955fc75df2bc23f19ee9f5
Joe Gallego <josephjgallego@gmail.com>
2020-05-14 14:46:51 -0700

    practice

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -12,20 +12,40 @@ public class ProblemSolving {
 
     int[] arraySecond = new int[]{4, 1, 2, 10, 5, 20};
 
+    // assume: both arrays same length
+
     // task: find the pairs of numbers, with one number from each array, that get closest to 24 when added together
 
     // pairs: (5,20) and (3,20)
 
     // #2 think of a simpler version of the problem
 
-    
+    // take first set and compare each in second set for closest to 24
+    // O(n)
+    // O(x*n)
 
     // #3 think with simpler examples- try noticing a pattern
 
+    int[] arrayThird = new int[]{7, 4, 1, 10};
+    int[] arrayFourth = new int[]{4, 5, 8, 7};
+    // target: 13
+
+    /*
+        4   5   8   7
+    7   11  12  15  14
+    4   8   9   12  11
+    1   5   6   9   8
+    10  14  15  18  17
+
+    */
+
     // #4 use some visualization
+
 
     // #5 test your solution on a few examples
 
+    // Time:    O(nlogn)
+    // Space:   O(n)
 
 
 
