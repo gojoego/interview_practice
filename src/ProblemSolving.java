edf217e258692e26656c070144a497e0b3f76745
Joe Gallego <josephjgallego@gmail.com>
2020-05-14 12:27:20 -0700

    practice

--- src/ProblemSolving.java
+++ src/ProblemSolving.java
@@ -1,9 +1,35 @@
+import java.lang.reflect.Array;
+
 public class ProblemSolving {
 
     public static void main(String[] args) {
 
     }
 
+    // #1 come up with a brute-force solution
+
+    int[] arrayFirst = new int[]{-1, 3, 8, 2, 9, 5};
+
+    int[] arraySecond = new int[]{4, 1, 2, 10, 5, 20};
+
+    // task: find the pairs of numbers, with one number from each array, that get closest to 24 when added together
+
+    // pairs: (5,20) and (3,20)
+
+    // #2 think of a simpler version of the problem
+
     
 
+    // #3 think with simpler examples- try noticing a pattern
+
+    // #4 use some visualization
+
+    // #5 test your solution on a few examples
+
+
+
+
 }
+
+
+// from CS Dojo: https://www.youtube.com/watch?v=GBuHSRDGZBY
