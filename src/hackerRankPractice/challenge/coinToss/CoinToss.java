0bacba2637af4142208f33b27f7bddc284383263
Joe Gallego <josephjgallego@gmail.com>
2020-05-19 14:51:11 -0700

    practice

--- src/hackerRankPractice/challenge/coinToss/CoinToss.java
+++ src/hackerRankPractice/challenge/coinToss/CoinToss.java
@@ -6,10 +6,37 @@ public class CoinToss {
 
     public static void main(String[] args) {
 
+        CoinToss game = new CoinToss();
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
+        System.out.println(game.tossACoin());
     }
 
-    public int tossACoin(){
+    public String tossACoin() {
         Random random = new Random();
+        int toss = Math.abs(random.nextInt()) % 2;
+        if (toss == 0) {
+            return "Heads";
+        } else {
+            return "Tails";
+        }
+    }
+}
+
+/*
+
+class: blueprint with tools (properties and methods)
+
+instance: an object created from the blueprint
+
+if (condition){
+    condition true: do this
+    } else {
+    condition false: do this
     }
 
-}
+*/
