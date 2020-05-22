9040a5c65f1106762f72f8d53aba368418a0794e
Joe Gallego <josephjgallego@gmail.com>
2020-05-21 17:55:43 -0700

    practice

--- src/hackerRankPractice/challenge/guessTheNumber/GuessTheNumber.java
+++ src/hackerRankPractice/challenge/guessTheNumber/GuessTheNumber.java
@@ -4,7 +4,15 @@ package hackerRankPractice.challenge.guessTheNumber;
 
 class method: no instance needed to use this function
 
+how to play:    1. computer picks random number
+                2. user guesses
+                3. computer gives clues
+                4. we guess until we get it
+
 */
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessTheNumber {
 
@@ -19,6 +27,26 @@ public class GuessTheNumber {
         howBigIsMyNumber(-1);
         howBigIsMyNumber(105);
 
+    }
+
+    int theNUMBER;
+    int max;
+    Scanner scanner = new Scanner(System.in);
+
+    public GuessTheNumber() {
+        Random random = new Random();
+        theNUMBER = Math.abs(random.nextInt()) % (max + 1);
+    }
+
+    public void play(){
+        int move = scanner.nextInt();
+        if (move > theNUMBER){
+            System.out.println("Your number is too big");
+        } else if (move < theNUMBER){
+            System.out.println("Your number is too small");
+        } else {
+            System.out.println("YOU GOT IT!");
+        }
     }
 
     public static void howBigIsMyNumber(int x){
