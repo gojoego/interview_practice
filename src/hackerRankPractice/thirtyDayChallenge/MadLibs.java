3e385b0735aca79e15c56483fdd50bb08a1afeaf
Joe Gallego <josephjgallego@gmail.com>
2020-05-25 16:22:20 -0700

    practice

---
+++ src/hackerRankPractice/thirtyDayChallenge/MadLibs.java
@@ -0,0 +1,13 @@
+package hackerRankPractice.thirtyDayChallenge;
+
+/*
+
+mad libs:
+1. a story with blanks
+2. you fill in the blanks without knowing the big picture
+3. we get a funny story
+
+*/
+
+public class MadLibs {
+}
