b7e763f750a853635248035a6f712168489f2964
Joe Gallego <josephjgallego@gmail.com>
2020-05-27 13:37:24 -0700

    updates

--- src/hackerRankPractice/thirtyDayChallenge/ArrayPractice.java
+++ src/hackerRankPractice/thirtyDayChallenge/ArrayPractice.java
@@ -11,6 +11,32 @@ array:  a container object that holds a fixed number of values that have a singl
 
 public class ArrayPractice {
 
+    public static void printArray(int[] array){
+        System.out.print("[");
+        // retrieving items from array using index
+        for (int i = 0; i < array.length; i++){
+            int item = array[i];
+            System.out.print(item);
+            if (i < array.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void printArray(String[] array){
+        System.out.print("[");
+        // retrieving items from array using index
+        for (int i = 0; i < array.length; i++){
+            String item = array[i];
+            System.out.print(item);
+            if (i < array.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
     public static void main(String[] args) {
 
         // Index  0  1  2  3  4
@@ -28,6 +54,16 @@ public class ArrayPractice {
         // 2nd way to initialize an array
 
         int[] intArray2 = new int[4];
+        System.out.println("array before complete initialization:");
+        printArray(intArray2);
+        System.out.println();
+
+        // initializing intArray2
+        // name[index] = desiredValue
+        intArray2[0] = 10;
+        intArray2[1] = 8;
+        intArray2[2] = 5;
+        intArray2[3] = 10;
 
         // 3rd way to initialize an array
 
@@ -36,15 +72,36 @@ public class ArrayPractice {
         String[] shoppingList = {"bananas", "apples", "pears"};
 
         // print out Arrays
-        System.out.println(Arrays.toString(intArray2));
-        System.out.println(Arrays.toString(intArray3));
+        System.out.println("2nd int Array: " + Arrays.toString(intArray2));
+        System.out.println("3rd int Array: " + Arrays.toString(intArray3));
+        System.out.println();
 
-        // initializing intArray2
-        // name[index] = desiredValue
-        intArray2[0] = 10;
-        intArray2[1] = 8;
-        intArray2[2] = 5;
-        intArray2[3] = 10;
+        // custom print out arrays
+        System.out.println("using custom print:");
+        printArray(intArray2);
+        printArray(intArray3);
+        System.out.println();
+
+        // retrieve objects
+        System.out.println("value of third index in 2nd int Array: " + intArray2[3]);
+        System.out.println();
+
+        // given functions
+        Arrays.sort(intArray3);
+        System.out.println("after using given sort function:");
+        printArray(intArray3);
+        System.out.println();
+
+        // print String array
+        System.out.println("using printArray function for Strings:");
+        printArray(shoppingList);
+
+        // special for loop: foreach loop
+        System.out.println("Special For Loop:");
+        for (String s : shoppingList){
+            System.out.print(s);
+            System.out.print(" ");
+        }
     }
 }
 
