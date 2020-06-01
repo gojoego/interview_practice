6c23de05069803ffa544c2ea90f90d20b335035f
Joe Gallego <josephjgallego@gmail.com>
2020-05-31 23:55:08 -0700

    updates

--- src/hackerRankPractice/thirtyDayChallenge/Recursion.java
+++ src/hackerRankPractice/thirtyDayChallenge/Recursion.java
@@ -65,19 +65,21 @@ public class Recursion {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int result = factorial(n);
+        System.out.println(result);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
 
         scanner.close();
+
     }
 /*
 
