9d6726eff14d6752fbbafec5fad76288cc745a5c
Joe Gallego <josephjgallego@gmail.com>
2020-06-03 14:48:43 -0700

    updates

---
+++ src/hackerRankPractice/thirtyDayChallenge/LibraryCatalog.java
@@ -0,0 +1,23 @@
+package hackerRankPractice.thirtyDayChallenge;
+
+/*
+
+What's in a catalog?
+
+1. what's available?
+2. check in a book
+3. check out a book
+4. container for books
+5. is a given book returned late?
+6. is a book checked out?
+
+*/
+
+public class LibraryCatalog {
+}
+
+/*
+
+
+
+*/
