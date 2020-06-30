21ac8b3811c5be9b21bbeb4ceb473acefc7e7224
Joe Gallego <josephjgallego@gmail.com>
2020-06-30 14:30:03 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/animal/Animal.java
+++ src/hackerRankPractice/thirtyDayChallenge/animal/Animal.java
@@ -1,4 +1,32 @@
 package hackerRankPractice.thirtyDayChallenge.animal;
 
+/*
+
+what is inheritance?
+-allows you to build new classes from existing classes
+-reuse code
+-defines the relationship between the superclass and the subclass
+
+traits:
+parent superclass = has the trait originally
+child subclass = inherits the trait
+
+* everything in code that is referenced inherits from object
+* object is the superclass of things we create in java
+
+object = superclass
+animal = subclass
+
+*/
+
 public class Animal {
+
+    public Animal(){}
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+
+    }
+
 }
