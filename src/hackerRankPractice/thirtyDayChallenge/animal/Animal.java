9e27265e2597b0a87a8691d2ff7766baf6192c0a
Joe Gallego <josephjgallego@gmail.com>
2020-07-01 18:40:16 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/animal/Animal.java
+++ src/hackerRankPractice/thirtyDayChallenge/animal/Animal.java
@@ -22,7 +22,7 @@ animal = subclass
 
 */
 
-public class Animal {
+public abstract class Animal {
 
     public int age; // vs private int age;
     // if making private, can utilize getters
@@ -33,8 +33,10 @@ public class Animal {
         System.out.println("An animal has been created!");
     }
 
-    public void eat(){
-        System.out.println("An animal is eating");
+    public abstract void eat();
+
+    public void sleep(){
+        System.out.println("An animal is sleeping.");
     }
 
     public int getAge() {
@@ -43,21 +45,72 @@ public class Animal {
 
     public static void main(String[] args) {
 
-        Animal a = new Animal(5);
+//        Animal a = new Animal(5);
         Dog d = new Dog();
         Cat c = new Cat();
-        d.ruff();
-        System.out.println(d.age);
-        c.meow();
-        System.out.println(c.age);
-        a.eat();
         d.eat();
         c.eat();
-        d.run();
-        c.prance();
+        d.sleep();
+        c.sleep();
+
+        // casting
+        Object dog = new Dog();
+        Dog realDog = (Dog) dog;
+        realDog.ruff();
+
+        Object str = "est";
+        String realS = (String) str;
+        realS.getBytes();
+        // usually less defined to something more specific
+
+        // what happens when...
+        Dog doggy = new Dog();
+        if (d instanceof Animal){
+            Animal animal = (Animal) doggy;
+            animal.sleep();
+        }
+        doggy.sleep();
+
+//        d.ruff();
+//        System.out.println(d.age);
+//        c.meow();
+//        System.out.println(c.age);
+//        a.eat();
+//        d.eat();
+//        c.eat();
+//        d.run();
+//        c.prance();
 
         // what happens if...
-        Animal o = new Dog();
+//        Animal o = new Dog();
         // cannot... Dog o = new Animal(3);
     }
 }
+
+/*
+
+what are we doing?
+-abstract classes
+-abstract methods
+-instance of
+-type casting
+
+what is an abstract class?
+1. class with at least one abstract method, a method declared without an implementation
+2. may have abstract and non-abstract methods
+3. cannot be initialized
+
+* abstract methods do NOT have a body
+
+* both abstract and non-abstract classes can override methods
+
+abstract classes vs. overriding methods:
+-classes that extend abstract classes must implement abstract methods or be abstract themselves
+-with abstract classes, a method may not be defined, the program does not know what to do
+-with overriding methods, there is an implementation at a superclass level
+-implemented method in superclass: may or may not be overridden in subclass
+
+type casting
+-casting: converting data type to another data type
+
+*/
