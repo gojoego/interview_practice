92b1ddd0478499817baf8b4169ecd02bbb0fde4e
Joe Gallego <josephjgallego@gmail.com>
2020-07-01 23:23:31 -0500

    practice

---
+++ src/hackerRankPractice/thirtyDayChallenge/calendarPractice/CalendarPractice.java
@@ -0,0 +1,31 @@
+package hackerRankPractice.thirtyDayChallenge.calendarPractice;
+
+/*
+
+what are we doing?
+-scope
+-imports
+-packages
+-figuring out why the stuff we've been doing works
+
+package: group of similar classes
+
+types of packages
+1. user-defined packages
+2. built-in packages
+
+* import packages to get access to classes, methods, properties, etc
+
+* built in - do not have to download or make anything new to use it
+
+* import al needed packages except java.lang
+
+
+
+*/
+
+public class CalendarPractice {
+}
+
+/*
+*/
