35bd7c3431487409dbd86ad92640fe6955606e56
Joe Gallego <josephjgallego@gmail.com>
2020-07-04 14:47:14 -0500

    practice

---
+++ src/hackerRankPractice/thirtyDayChallenge/Exceptions.java
@@ -0,0 +1,24 @@
+package hackerRankPractice.thirtyDayChallenge;
+
+/*
+
+
+
+*/
+
+public class Exceptions {
+}
+
+/*
+
+STRING TO INTEGER
+Many languages offer some functionality for parsing an integer value from a string token.
+
+Each of these functions will raise an error (i.e.: throw an exception) when the argument
+passed as the token parameter cannot be converted to an integer. Most languages have built-in
+exception handling methods such as try-catch or begin-rescue that enable you to code
+specific behaviors to handle execution issues.
+
+
+
+*/
