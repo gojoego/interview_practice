fed7a24c95ed4e08ff694e53c55f226c049b1278
Joe Gallego <josephjgallego@gmail.com>
2020-07-05 17:16:30 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/hotChocolate/HotChocolate.java
+++ src/hackerRankPractice/thirtyDayChallenge/hotChocolate/HotChocolate.java
@@ -1,4 +1,34 @@
 package hackerRankPractice.thirtyDayChallenge.hotChocolate;
 
+/*
+
+what if a method does not handle an exception?
+
+method must declare it using "throws" keyword
+
+if exception might arise...
+1. use try-catch block
+2. use keyword "throws" in method signature
+    -states that we will handle error higher up in the program
+
+we can also throw errors!
+- we can throw error inside of method using "throw" when
+  we know something is wrong with the program (even if Java
+  does not know something is wrong)
+
+for example: programming a game
+rule 1: score should never be negative
+rule 2: there should be no way to lose points
+if we break rule, throw exception
+stop invalid games via exceptions
+
+java's exceptions only arise if there are...
+1. syntax errors
+2. logic errors/bad access errors
+
+
+
+*/
+
 public class HotChocolate {
 }
