9215840cd938015bcc1b7b3cfb79ffb669554cfc
Joe Gallego <josephjgallego@gmail.com>
2020-07-05 19:41:54 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/MoreExceptions.java
+++ src/hackerRankPractice/thirtyDayChallenge/MoreExceptions.java
@@ -3,23 +3,29 @@ package hackerRankPractice.thirtyDayChallenge;
 /*
 
 Objective
-Yesterday's challenge taught you to manage exceptional situations by using try and catch blocks. In today's challenge, you're going to practice throwing and propagating an exception. Check out the Tutorial tab for learning materials and an instructional video!
 
 Task
-Write a Calculator class with a single method: int power(int,int). The power method takes two integers,  and , as parameters and returns the integer result of . If either  or  is negative, then the method must throw an exception with the message: n and p should be non-negative.
+Write a Calculator class with a single method: int power(int,int).
+The power method takes two integers, n and p, as parameters and
+returns the integer result of n^p. If either n or p is negative,
+then the method must throw an exception with the message: n and p should be non-negative.
 
 Note: Do not use an access modifier (e.g.: public) in the declaration for your Calculator class.
 
 Input Format
 
-Input from stdin is handled for you by the locked stub code in your editor. The first line contains an integer, , the number of test cases. Each of the  subsequent lines describes a test case in  space-separated integers denoting  and , respectively.
+Input from stdin is handled for you by the locked stub code in your editor.
+The first line contains an integer, T, the number of test cases. Each of the
+T subsequent lines describes a test case in 2 space-separated integers
+denoting n and p, respectively.
 
-Constraints
+Constraints: No Test Case will result in overflow for correctly written code.
 
-No Test Case will result in overflow for correctly written code.
 Output Format
 
-Output to stdout is handled for you by the locked stub code in your editor. There are  lines of output, where each line contains the result of  as calculated by your Calculator class' power method.
+Output to stdout is handled for you by the locked stub code in your editor.
+There are T lines of output, where each line contains the result of n^p as
+calculated by your Calculator class' power method.
 
 Sample Input
 
@@ -28,29 +34,79 @@ Sample Input
 2 4
 -1 -2
 -1 3
+
 Sample Output
 
 243
 16
 n and p should be non-negative
 n and p should be non-negative
+
 Explanation
 
-
-:  and  are positive, so power returns the result of , which is .
-:  and  are positive, so power returns the result of =, which is .
-: Both inputs ( and ) are negative, so power throws an exception and  is printed.
-: One of the inputs () is negative, so power throws an exception and  is printed.
+T = 4
+T0: 3 and 5 are positive, so power returns the result of 3^5, which is 243.
+T1: 2 and 4 are positive, so power returns the result of 2^4=, which is 16.
+T2: Both inputs (-1 and -2) are negative, so power throws an exception and
+ "n and p should be non-negative" is printed.
+T3: One of the inputs (-1) is negative, so power throws an exception and
+ "n and p should be non-negative" is printed.
 
 */
 
+import java.util.Scanner;
+
+// write Calculator class
+class Calculator {
+
+    // method power(int n, int p)
+    // if negative: "n and p should be non-negative"
+    public int power(int n, int p) {
+        // if-else statement if negative number
+        if (n < 0){
+            throw new NumberFormatException("n and p should be non-negative");
+        } else if (p < 0){
+            throw new NumberFormatException("n and p should be non-negative");
+        } else {
+            return (int) Math.pow(n, p);
+        }
+    }
+}
+
 public class MoreExceptions {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n,p);
+                System.out.println(ans);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
 }
 
 /*
 
-Propagating Exceptions
-If an exception is not caught by the method that threw it, the program's control is transferred back (propagated) to the calling method (i.e.: whatever called the method that threw the exception). This can be good if you have designed your program to handle exceptions at a high level, but bad if you never write code to catch the exception in the calling methods that the exception is being propagated to. The example method below throws an exception of type Exception, which propagates back to the calling method (main), where a catch block catches it and prints a message:
+PROPAGATING EXCEPTIONS
+If an exception is not caught by the method that threw it,
+the program's control is transferred back (propagated) to
+the calling method (i.e.: whatever called the method that
+threw the exception). This can be good if you have designed
+your program to handle exceptions at a high level, but bad
+if you never write code to catch the exception in the calling
+methods that the exception is being propagated to. The example
+method below throws an exception of type Exception, which
+propagates back to the calling method (main), where a catch
+block catches it and prints a message:
 
 class PropagatedException {
 
