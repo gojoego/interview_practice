e6de371b413c200b73c6ee5b30531f7af4510cbf
Joe Gallego <josephjgallego@gmail.com>
2020-07-09 21:32:23 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/Interfaces.java
+++ src/hackerRankPractice/thirtyDayChallenge/Interfaces.java
@@ -45,10 +45,10 @@ class Calculator2 implements AdvancedArithmetic {
     public int divisorSum(int n){
         int sum =0;
         for (int i = 1; i <= n; i++){
-            if (n % i != 0){
-                continue;
-            } if (n%i==0){
+            if (n % i == 0){
                 sum = sum + n;
+            } else {
+                break;
             }
         }
         return sum;
