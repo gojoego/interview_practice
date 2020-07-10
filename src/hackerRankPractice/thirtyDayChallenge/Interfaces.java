8f8156d46e857b36a36aa7149a36027e6f4629b2
Joe Gallego <josephjgallego@gmail.com>
2020-07-09 23:32:32 -0500

    practice

--- src/hackerRankPractice/thirtyDayChallenge/Interfaces.java
+++ src/hackerRankPractice/thirtyDayChallenge/Interfaces.java
@@ -46,9 +46,9 @@ class Calculator2 implements AdvancedArithmetic {
         int sum =0;
         for (int i = 1; i <= n; i++){
             if (n % i == 0){
-                sum = sum + n;
+                sum = sum + i;
             } else {
-                break;
+                continue;
             }
         }
         return sum;
