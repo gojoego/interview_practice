60f71ad1369af83d8d0ccfbbd5b3b005d015cef9
Joe Gallego <josephjgallego@gmail.com>
2020-07-21 20:57:36 -0500

    updates

---
+++ src/hackerRankPractice/thirtyDayChallenge/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,57 @@
+package hackerRankPractice.thirtyDayChallenge.binarySearchTree;
+
+/*
+
+more data structures: graphs and trees
+-more terms
+-recursive
+
+when we draw tree, we draw a root
+-node
+-element of tree
+
+trees
+-root
+    has two children
+-leaves or children
+-can add children to children
+-the children are trees, sub-trees
+
+1. node as child
+2. subtree as child
+3. no child
+
+there are different ways of organizing data with a tree
+what makes something a child vs a parent
+
+heap
+-root has special relations with children
+-but children have no relationship with each other
+
+1. min-heap:
+    -smaller data
+    -smallest element at root
+    -everything below is bigger than root
+    -root is smaller than children
+    -does not mean 1 child smaller than other
+    -order does not matter
+    -as long as they are smaller than parent
+    -example: golf
+
+2. max-heap:
+    =opposite of min heap
+    -largest element at root
+    -root is largest element
+    -
+
+
+*/
+
+public class BinarySearchTree {
+}
+
+/*
+
+
+
+*/
