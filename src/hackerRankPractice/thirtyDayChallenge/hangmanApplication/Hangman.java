c9b3015f9604afa95bb10739c5a632cb58613fc4
Joe Gallego <josephjgallego@gmail.com>
2020-08-04 13:26:20 -0500

    updates

--- src/hackerRankPractice/thirtyDayChallenge/hangmanApplication/Hangman.java
+++ src/hackerRankPractice/thirtyDayChallenge/hangmanApplication/Hangman.java
@@ -28,30 +28,201 @@ public class Hangman {
         mysteryWord = pickWord();
         currentGuess = initializeCurrentGuess();
     }
+
     public void initializeStreams() throws IOException {
+
         try {
-            File inFile = new File("dictionary.txt");
+            File inFile = new File("/dictionary.txt");
             fileReader = new FileReader(inFile); // reads file
             bufferedFileReader = new BufferedReader(fileReader); // allows parsing
             String currentLine = bufferedFileReader.readLine();
-            while (currentLine != null){
+            while (currentLine != null) {
                 dictionary.add(currentLine); // adding words to dictionary
+                currentLine = bufferedFileReader.readLine(); // allows incrementing of current line
             }
             bufferedFileReader.close();
             fileReader.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Could not initialize streams");
         }
     }
-    public String pickWord(){
+
+    public String pickWord() {
         Random random = new Random();
         int wordIndex = Math.abs(random.nextInt()) % dictionary.size();
         return dictionary.get(wordIndex);
     }
 
     // _ _ _ _ _ _ _
-    public StringBuilder initializeCurrentGuess(){
+    public StringBuilder initializeCurrentGuess() {
         StringBuilder current = new StringBuilder();
-        for (i)
+        for (int i = 0; i < mysteryWord.length() * 2; i++) {
+            if (i % 2 == 0) {
+                current.append("_");
+            } else {
+                current.append(" ");
+            }
+        }
+        return current;
+    }
+
+    // _ _ A _ _ B
+    public String getFormalCurrentGuess() {
+        return "Current Guess: " + currentGuess.toString(); // allows us to return current guess in String format
+    }
+
+//            " - - - - -\n"+
+//            "|        |\n"+
+//            "|        O\n" +
+//            "|      / | \\ \n"+
+//            "|        |\n" +
+//            "|       / \\ \n" +
+//            "|\n" +
+//            "|\n";
+
+    public boolean gameOver() {
+        if (didWeWin()){
+            System.out.println();
+            System.out.println("Congrats! You won! You guessed the right word!");
+        } else if (didWeLose()){
+            System.out.println();
+            System.out.println("Sorry, you lost. You spent all of your 6 tries. " +
+                    "The word was " + mysteryWord + ".");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean didWeLose() {
+        return currentTry >= maxTries;
+    }
+
+    private boolean didWeWin() {
+        String guess = getCondensedCurrentGuess();
+        return guess.equals(mysteryWord);
+    }
+
+    private String getCondensedCurrentGuess() {
+        String guess = currentGuess.toString();
+        return guess.replace(" ","");
+    }
+
+
+
+    public boolean isGuessedAlready(char guess) {
+        return previousGuesses.contains(guess);
+    }
+
+    public boolean playGuess(char guess) {
+        boolean isItAGoodGuess = false;
+        for (int i = 0; i < mysteryWord.length(); i++) {
+            if (mysteryWord.charAt(i) == guess) {
+                currentGuess.setCharAt(i * 2, guess);
+                isItAGoodGuess = true;
+                previousGuesses.add(guess);
+            }
+        }
+        if (!isItAGoodGuess) {
+            currentTry++;
+        }
+
+        return isItAGoodGuess;
+    }
+
+    public String drawPicture() {
+        switch (currentTry) {
+            case 0:
+                return noPersonDraw();
+            case 1:
+                return addHeadDraw();
+            case 2:
+                return addBodyDraw();
+            case 3:
+                return addOneArmDraw();
+            case 4:
+                return addSecondArmDraw();
+            case 5:
+                return addOneLegDraw();
+            default:
+                return fullPersonDraw();
+        }
+    }
+
+
+    private String noPersonDraw() {
+        return " - - - - -\n" +
+                "|          \n" +
+                "|          \n" +
+                "|          \n" +
+                "|          \n" +
+                "|          \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String addHeadDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|          \n" +
+                "|          \n" +
+                "|          \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String addBodyDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|        |\n" +
+                "|        |\n" +
+                "|       \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String addOneArmDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|      / |  \n" +
+                "|        |\n" +
+                "|       / \\ \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String addSecondArmDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|      / | \\ \n" +
+                "|        |\n" +
+                "|        \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String addOneLegDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|      / | \\ \n" +
+                "|        |\n" +
+                "|       /  \n" +
+                "|\n" +
+                "|\n";
+    }
+
+    private String fullPersonDraw() {
+        return " - - - - -\n" +
+                "|        |\n" +
+                "|        O\n" +
+                "|      / | \\ \n" +
+                "|        |\n" +
+                "|       / \\ \n" +
+                "|\n" +
+                "|\n";
     }
 }
--- src/hackerRankPractice/thirtyDayChallenge/hangmanApplication/HangmanApplication.java
+++ src/hackerRankPractice/thirtyDayChallenge/hangmanApplication/HangmanApplication.java
@@ -13,34 +13,67 @@ so what are we doing?
 
 */
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HangmanApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // how do we play the game
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to hangman! I will pick a word and you will try to guess it character " +
-                "by character. If you guess wrong 6 times, then I win. If you can guess it before then, you win. " +
+                "by character. \n If you guess wrong 6 times, then I win. \n If you can guess it before then, you win. " +
                 "Are you ready? I hope so because I am.");
         System.out.println();
         System.out.println("I have picked my word. Below is a picture, and below " +
-                "that is your current guess, which starts off as nothing. Every time you " +
-                "guess incorrectly. I add a body part to the picture. When there is a full" +
+                "that is your current guess, which starts off as nothing. \n Every time you " +
+                "guess incorrectly. I add a body part to the picture. \n When there is a full" +
                 " person, you lose.");
 
         // allows for multiple games
         boolean doYouWantToPlay = true;
         while (doYouWantToPlay){
             // setting up the game
-            while (!game.gameOver()){
-                // playing the game
-            }
-            // playing again or no?
             System.out.println();
-            System.out.println("Do you want to play another game? Enter Y if you do.");
-            Character response = (scanner.next().toUpperCase()).charAt(0);
-            doYouWantToPlay = (response == 'Y');
+            System.out.println("Alright! Let's play!");
+            Hangman game = new Hangman();
+
+            do {
+                // draw the things...
+                System.out.println("");
+                System.out.println(game.drawPicture());
+                System.out.println();
+                System.out.println(game.getFormalCurrentGuess());
+                System.out.println(game.mysteryWord);
+                System.out.println();
+
+                // get the guess
+                System.out.println("Enter a character that you think is in the word.");
+                char guess = (scanner.next().toLowerCase()).charAt(0);
+                System.out.println();
+
+                // check if the character is guessed already
+                if (game.isGuessedAlready(guess)){
+                    System.out.println("Try again! You've already guessed that character.");
+                    guess = (scanner.next().toLowerCase()).charAt(0);
+                }
+
+                // play the guess
+                if (game.playGuess(guess)){
+                    System.out.println("Great guess! That character is in the word!");
+                }else {
+                    System.out.println("Unfortunately that character isn't int he word");
+                }
+            }
+            while (!game.gameOver());
+                // playing the game
+
+                // playing again or no?
+                System.out.println();
+                System.out.println("Do you want to play another game? Enter Y if you do.");
+                Character response = (scanner.next().toUpperCase()).charAt(0);
+                doYouWantToPlay = (response == 'Y');
+
         }
     }
 }
